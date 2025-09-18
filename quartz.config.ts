e1605be3bd03185618b22f7eb7029c8bd3a61fcd
hyunkyo kim <kuzwolka@gmail.com>
2025-09-18 14:22:28 +0900

    Quartz sync: Sep 18, 2025, 2:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-MN4HZJLN9W",
     },
     locale: "ko-KR",
     baseUrl: "kuzblog.pages.dev",
